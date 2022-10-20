be1765dbf6fb1bbb89b1c49886851c81ad1d1e06
KhantsevaAA <3104nastya@gmail.com>
2022-10-20 09:47:58 +0300

    add tasks 2 and 3 level

--- Lab3/Lab3.3.12-13-14/Program.cs
+++ Lab3/Lab3.3.12-13-14/Program.cs
@@ -10,6 +10,69 @@ namespace Lab3._3._12_13_14
     {
         static void Main(string[] args)
         {
+            int lmax = 100, n = 0;
+            double[] mas = new double[lmax];
+            bool f = true;
+            string str;
+            Console.WriteLine("Лабораторная работа №3. Сложность 2\n" +
+                $"Введите элементы массива через Enter не более {lmax}, для окончания введите 'end' (после некорректного ввода, 'end' программа не примет:)");
+            do
+            {
+                Console.Write($" Элемент {n + 1}: ");
+                str = Console.ReadLine();
+                if (str == "end")
+                    f = false;
+                else
+                {
+                    while (!double.TryParse(str.Replace('.', ','), out mas[n]))
+                    {
+                        Console.Write($"  Упс! Некорректное значение. Попробуйте ещё раз. Элемент {n}: ");
+                        str = Console.ReadLine();
+                    }
+                    n = n + 1;
+                }
+
+            }
+            while (f == true);
+
+            int n1 = n;
+            Console.WriteLine("Задание 12");
+            for (int i=0; i<n; i++)
+                if (mas[i] < 0)
+                {
+                    Console.WriteLine($"{i} {mas[i]}");
+                    n1 -= 1;
+                    for (int j = i; j <= n1; j++)
+                        mas[j] = mas[j + 1];
+                }
+
+            if (n == n1)
+                Console.Write(" В массиве нет отрицательных элементов => Массив не изменён");
+            else
+            {
+                Console.Write(" Новый массив: ");
+                for (int i = 0; i < n1; i++)
+                    Console.Write($" {mas[i]}");
+            }
+
+            n = n1;
+            Console.WriteLine("\nЗадание 13");
+            for (int i = 0; i < n; i++)
+                for (int j=i+1;j<n;j++)
+                    if (mas[i] == mas[j])
+                    {
+                        n1 -= 1;
+                        for (int k = j; k <= n1; k++)
+                            mas[k] = mas[k + 1];
+                    }
+            if (n == n1)
+                Console.Write(" В массиве нет повторяющихся элементов => Массив не изменён");
+            else
+            {
+                Console.Write(" Новый массив: ");
+                for (int i = 0; i <= n1; i++)
+                    Console.Write($" {mas[i]}");
+            }
         }
     }
 }
