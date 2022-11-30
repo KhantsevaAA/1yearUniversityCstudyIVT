a775a76e9e54ce56ee90a26398c6951425928740
KhantsevaAA <3104nastya@gmail.com>
2022-11-30 18:00:55 +0300

    modified Lab5

--- 5LAB/Lab5/Lab.5.2.26-27-28/Program.cs
+++ 5LAB/Lab5/Lab.5.2.26-27-28/Program.cs
@@ -6,7 +6,9 @@ using System.Globalization;
 using System.Linq;
 using System.Net.NetworkInformation;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace Lab._5._2._26_27_28
 {
@@ -93,14 +95,14 @@ namespace Lab._5._2._26_27_28
             for (int j = 0; j < x.GetLength(1); j++)
             {
                 x[0, j] = r.Next((int)Math.Min(a, b) * 10, (int)Math.Max(a, b) * 10) * 0.1;
-                x[0, j] = j;
+                //x[0, j] = j;
             }
             for (int i = 1; i < x.GetLength(0); i++)
                 for (int j = 0; j < x.GetLength(1); j++)
                 {
                     x[i, j] = r.Next(-1000, 1000) * 0.1;
-                    x[1, j] = x.GetLength(1)-j;
-                    x[2, j] = 1;
+                    //x[1, j] = x.GetLength(1)-j;
+                    //x[2, j] = 1;
                 }
         }
 
@@ -153,7 +155,7 @@ namespace Lab._5._2._26_27_28
                 n = 0;
                 if (x[i, n] == x[i, n + 1])
                     while (x[i, n] == x[i, n + 1] && n < x.GetLength(1) - 2)
-                        n = n + 1;
+                        n++;
                 //Console.WriteLine($"{n} {x.GetLength(1)}");
                 if (n + 1 == x.GetLength(1) - 1)
                 {
@@ -167,13 +169,13 @@ namespace Lab._5._2._26_27_28
                     if (x[i, n] > x[i, n + 1])
                     {
                         while ( n < x.GetLength(1) - 1 && x[i, n] >=x[i, n + 1])
-                            n = n + 1;
+                            n++;
                         f = true;
                     }
-                    else //if (x[i, n] < x[i, n + 1])
+                    else
                     {
                         while ( n < x.GetLength(1) - 1 && x[i, n] <= x[i, n + 1])
-                            n = n + 1;
+                            n++;
                         f = false;
                     }
                     Console.WriteLine($"{n}");
@@ -198,74 +200,106 @@ namespace Lab._5._2._26_27_28
             }
             return str1 + " и " + str2;
         }
-
+        static string changes(int i, string s, double[,] x)
+        {
+            Console.WriteLine($" Промежутки монотонности y{i}:");
+            int n = 0,m,max=0,maxn=0;
+            while (n < x.GetLength(1) - 1)
+            {
+                if (x[i, n] >= x[i, n + 1])
+                {
+                    m = 0;
+                    while (n < x.GetLength(1) - 1 && x[i, n] >= x[i, n + 1])
+                    {
+                        n++;
+                        m++;
+                    }
+                    Console.WriteLine($"  монотоно убывает на (x{n - m}; x{n})");
+                    if (m >= max)
+                    {
+                        max = m;
+                        maxn = n;
+                    }
+                }
+                else
+                { 
+                    m = 0;
+                    while (n < x.GetLength(1) - 1 && x[i, n] <= x[i, n + 1])
+                    {
+                        n++;
+                        m++;
+                    }
+                    Console.WriteLine($"  монотоно возрастает на (x{n - m}; x{n})");
+                    if (m >= max)
+                    {
+                        max = m;
+                        maxn = n;
+                    }
+                }
+            }
+            return $"(x{maxn - max}; x{maxn})";            
+        }
+        
         static void mon(double[,] x, string str)
         {
-            int n, m, max = 0; ;
+            Console.Write(" б)");
             string[] s = str.Split(' ');
-            //for (int i = 0; i < s.Length; i++) 
-            //    Console.Write($"s[{i}]={s[i]} ");
+            string len0, len1;
             if (s[0] == "не")
+                len0=changes(1, str, x);
+            else
             {
-                Console.WriteLine(" Промежутки монотонности y1:");
-                n = 0;
-                while (n < x.GetLength(1) - 1)
-                {
-                    if (x[1, n] >= x[1, n + 1])
-                    {
-                        m = 0;
-                        while (n < x.GetLength(1) - 1 && x[1, n] >= x[1, n + 1])
-                        {
-                            n = n + 1;
-                            m = m + 1;
-                        }
-                        Console.WriteLine($" монотоно возрастает на (x{n - m}; x{n})");
-                    }
-                    else
-                    {
-                        m = 0;
-                        while (n < x.GetLength(1) - 1 && x[1, n] <= x[1, n + 1])
-                        {
-                            n = n + 1;
-                            m = m + 1;
-                        }
-                        Console.WriteLine($" монотоно убывает на (x{n - m}; x{n})");
-                    }
-                }
+                Console.WriteLine(" y1 " + s[0] + " " + s[1] + " на [A,B]");
+                len0="(A,B)";
+            }
+            if (s[3] == "не")
+            {
+                len1= changes(2, str, x);
             }
             else
-                Console.WriteLine("y1 "+s[0]+" " + s[1] + " на [A,B]");
-            if (s[4] == "не")
             {
-                Console.WriteLine(" Промежутки монотонности y2:");
-                n = 0;
-                while (n < x.GetLength(1) - 1)
-                {
-                    if (x[2, n] >= x[2, n + 1])
-                    {
-                        m = 0;
-                        while (n < x.GetLength(1) - 1 && x[2, n] >= x[2, n + 1])
-                        {
-                            n = n + 1;
-                            m = m + 1;
-                        }
-                        Console.WriteLine($" монотоно возрастает на (x{n - m}; x{n})");
-                    }
+                Console.WriteLine("y2 " + s[3] + " " + s[4] + " на [A,B]");
+                len1 = "(A,B)";
+            }
+            Console.WriteLine(" в) Самый длинный интервал монотонности ф-ции y1=f(x) "+len0+ ". Cамый длинный интервал монотонности ф-ции y2=f(x) " 
+                + len1+"\n Самый длинный интервал монотонности ф-ций "+ maxmon(len0, len1)+" (показывает последние найденные наибольшие промежутки монотонности)");
+        }
+        static string maxmon(string x, string y)
+        {
+            string[] s1 = x.Split(' ');
+            string[] s2 = y.Split(' ');
+            int n1, n2;
+            switch ((x[1], y[1]))
+            {
+                case ('A', 'A'):
+                    return "(A,B), обе функции на ней монотоны";
+                    break;
+                case ('A', 'x'):
+                    return "(A,B) в ф-ции y2=f(x)";
+                    break;
+                case ('x', 'A'):
+                    return "(A,B) в ф-ции y1=f(x)";
+                    break;
+                case ('x', 'x'):
+                    string[] xs= x.Split(';');
+                    string[] ys= y.Split(';');
+                    int n, m;
+                    n = Math.Abs(int.Parse(Regex.Replace(xs[0], @"[^\d]", ""))- int.Parse(Regex.Replace(xs[1], @"[^\d]", "")));
+                    m = Math.Abs(int.Parse(Regex.Replace(ys[0], @"[^\d]", ""))- int.Parse(Regex.Replace(ys[1], @"[^\d]", "")));
+                    if (n == m)
+                        return $"есть и в y1=f(x), и в y2=f(x) " + x + " и " + y + " соответственно";
                     else
                     {
-                        m = 0;
-                        while (n < x.GetLength(1) - 1 && x[2, n] <= x[2, n + 1])
-                        {
-                            n = n + 1;
-                            m = m + 1;
-                        }
-                        Console.WriteLine($" монотоно убывает на (x{n - m}; x{n})");
+                        if (n > m)
+                            return " в y1=f(x) " + x;
+                        else
+                            return " в y2=f(x) " + y;
                     }
-                }
+                    break;
+                default:
+                    return "";
+                    break;
             }
-            else
-                Console.WriteLine("y2 "+s[3] + " " + s[4]+" на [A,B]");
-
         }
         static void Main(string[] args)
         {
@@ -318,10 +352,9 @@ namespace Lab._5._2._26_27_28
             Console.Write(" B = ");
             inputdouble(out B);
             Console.Write(" Сгененерированная таблица:");
-            //double[,] tab = new double[3, (int)(Math.Abs(A - B) + 1)];
-            double[,] tab = { { 1, 3, 3, 4, 5 }, { 1, -3, 3, -1, -9 }, { 9, 9, 7, 7, -1, } };
-            //generate(tab, A, B);
-
+            double[,] tab = new double[3, (int)(Math.Abs(A - B) + 1)];
+            //double[,] tab = { { 1, 3, 3, 4, 5 }, { 1, -3, 3, -1, -9 }, { 9, 9, -4, 7, -1, } };
+            generate(tab, A, B);
             outtab(tab);
             order(tab);
             Console.WriteLine("\n Упорядочим значения аргумета по возрастанию:");
@@ -332,7 +365,7 @@ namespace Lab._5._2._26_27_28
             }
             outtab(tab);
             string str = monall(tab);
-            Console.Write(" а) Функция y1 и y2 " + str+ " на отрезке [A, B] соотвественно\n б)");
+            Console.WriteLine(" а) Функция y1 и y2 " + str+ " на отрезке [A, B] соотвественно)");
             mon(tab, str);
 
         }
